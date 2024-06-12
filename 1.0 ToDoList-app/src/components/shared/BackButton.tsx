8a13c039bfad25937000a2739726fd71af2027c8
Elaine Muhombe <emuhombe@gmail.com>
2024-06-12 20:09:30 +0300

    feat(src/components/shared): Add ChevronLeftIcon

--- 1.0 ToDoList-app/src/components/shared/BackButton.tsx
+++ 1.0 ToDoList-app/src/components/shared/BackButton.tsx
@@ -1,8 +1,10 @@
 
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';  // Example icon
 
 export default function BackButton() {
   return (
-    <div>
+    <div className='bg-white border-radius-full w-20 h-20'>
+     <ChevronLeftIcon />
       
     </div>
   )
